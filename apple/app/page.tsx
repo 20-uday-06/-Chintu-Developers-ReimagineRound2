30a69364059ddf7d1c0858c8e4109dd1ca4be086
Uday Tyagi <uday_t@ce.iitr.ac.in>
2024-07-19 02:53:16 +0530

    nav-bar

--- apple/app/page.tsx
+++ apple/app/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import React, { useState } from "react";
-import { HoveredLink, Menu, MenuItem, ProductItem } from "../components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "@/components/ui/navbar-menu";
 import { cn } from "@/lib/utils";
+import Image from "next/image";
 
-export default function NavbarDemo() {
+export default function Home() {
   return (
-    <div className="relative w-full flex items-center justify-center">
+    <div id="div">
+      <div className="relative w-full flex items-center justify-center">
       <Navbar className="top-2" />
       <p className="text-black dark:text-white">
         The Navbar will show on top of the page
       </p>
+    </div>
     </div>
   );
 }
@@ -21,36 +24,36 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="Services">
+        <MenuItem setActive={setActive} active={active} item="Store">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/web-dev">Web Development</HoveredLink>
-            <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-            <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-            <HoveredLink href="/branding">Branding</HoveredLink>
+            <HoveredLink href="/web-dev">Mumbai</HoveredLink>
+            <HoveredLink href="/interface-design">Delhi</HoveredLink>
+            <HoveredLink href="/seo">Ghaziabad</HoveredLink>
+            <HoveredLink href="/branding">Kota</HoveredLink>
           </div>
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Products">
           <div className="  text-sm grid grid-cols-2 gap-10 p-4">
             <ProductItem
-              title="Algochurn"
+              title="I Phone 15"
               href="https://algochurn.com"
-              src="https://assets.aceternity.com/demos/algochurn.webp"
+              src="https://m.media-amazon.com/images/I/71d7rfSl0wL._AC_UF1000,1000_QL80_.jpg"
               description="Prepare for tech interviews like never before."
             />
             <ProductItem
-              title="Tailwind Master Kit"
+              title="I Phone 15 pro"
               href="https://tailwindmasterkit.com"
               src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
               description="Production ready Tailwind css components for your next project"
             />
             <ProductItem
-              title="Moonbeam"
+              title="Mac Book Air"
               href="https://gomoonbeam.com"
               src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
               description="Never write from scratch again. Go from idea to blog in minutes."
             />
             <ProductItem
-              title="Rogue"
+              title="MacBook pro"
               href="https://userogue.com"
               src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
               description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
@@ -59,10 +62,38 @@ function Navbar({ className }: { className?: string }) {
         </MenuItem>
         <MenuItem setActive={setActive} active={active} item="Pricing">
           <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/hobby">Hobby</HoveredLink>
-            <HoveredLink href="/individual">Individual</HoveredLink>
-            <HoveredLink href="/team">Team</HoveredLink>
-            <HoveredLink href="/enterprise">Enterprise</HoveredLink>
+            <HoveredLink href="/hobby">1 eye</HoveredLink>
+            <HoveredLink href="/individual">1 kidney</HoveredLink>
+            <HoveredLink href="/team">2/3rd liver</HoveredLink>
+            <HoveredLink href="/enterprise">Gpay</HoveredLink>
+          </div>
+        </MenuItem>
+        <MenuItem setActive={setActive} active={active} item="Accessories">
+          <div className="  text-sm grid grid-cols-2 gap-10 p-4">
+            <ProductItem
+              title="Cloth"
+              href="https://algochurn.com"
+              src="https://assets.aceternity.com/demos/algochurn.webp"
+              description="Prepare for tech interviews like never before."
+            />
+            <ProductItem
+              title="Mag safe"
+              href="https://tailwindmasterkit.com"
+              src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
+              description="Production ready Tailwind css components for your next project"
+            />
+            <ProductItem
+              title="Charger"
+              href="https://gomoonbeam.com"
+              src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
+              description="Never write from scratch again. Go from idea to blog in minutes."
+            />
+            <ProductItem
+              title="Air pods"
+              href="https://userogue.com"
+              src="https://images.unsplash.com/photo-1511025998370-7d59f82e9c8f?q=80&w=2053&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+              description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
+            />
           </div>
         </MenuItem>
       </Menu>
---
+++ apple/next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+module.exports = {
+    reactStrictMode: true,
+    images: {
+      domains: ['assets.aceternity.com','images.unsplash.com','store.storeimages.cdn-apple.com'],
+    },
+  };
+
+  
+  
+  
