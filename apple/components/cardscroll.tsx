f0b77a612d6c5cfb68687fe6d74a4cb1df53d8ba
Uday Tyagi <uday_t@ce.iitr.ac.in>
2024-07-22 11:43:12 +0530

    scroll img

--- apple/components/cardscroll.tsx
+++ apple/components/cardscroll.tsx
@@ -7,96 +7,96 @@ export default function HeroParallaxDemo() {
 }
 export const products = [
   {
-    title: "Moonbeam",
-    link: "https://gomoonbeam.com",
+    title: "Mac",
+    link: "https://www.apple.com/newsroom/images/product/imac/standard/Apple_imac-magickeyboardnum-magicmouse2-macos-wallpaper_08042020.jpg.og.jpg?202405161915",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+      "https://www.apple.com/newsroom/images/product/imac/standard/Apple_imac-magickeyboardnum-magicmouse2-macos-wallpaper_08042020.jpg.og.jpg?202405161915",
   },
   {
-    title: "Cursor",
-    link: "https://cursor.so",
+    title: "MacBook Air",
+    link: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/mba13-midnight-select-202402?wid=904&hei=840&fmt=jpeg&qlt=90&.v=1708367688034",
   },
   {
-    title: "Rogue",
-    link: "https://userogue.com",
+    title: "MacBook Pro",
+    link: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKtAy-sFVjDXrUBqEUW4OeQJumsZu67zt6lA&s",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKtAy-sFVjDXrUBqEUW4OeQJumsZu67zt6lA&s",
   },
 
   {
-    title: "Editorially",
+    title: "Mac mini",
     link: "https://editorially.org",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+      "https://www.apple.com/v/mac-mini/t/images/meta/mac-mini_overview__164prubpwpee_og.jpg",
   },
   {
-    title: "Editrix AI",
-    link: "https://editrix.ai",
+    title: "MacBook Air m2",
+    link: "https://support.apple.com/en-in/111867",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+      "https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111867_SP869-2022-macbook-air-m2-colors.png",
   },
   {
-    title: "Pixel Perfect",
-    link: "https://app.pixelperfect.quest",
+    title: "iPhone SE",
+    link: "https://www.apple.com/newsroom/2020/04/iphone-se-a-powerful-new-smartphone-in-a-popular-design/",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+      "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_new-iphone-se-white_04152020_big.jpg.large.jpg",
   },
 
   {
-    title: "Algochurn",
-    link: "https://algochurn.com",
+    title: "iPhone 15",
+    link: "https://www.apple.com/in/newsroom/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+      "https://www.apple.com/newsroom/images/2023/09/apple-debuts-iphone-15-and-iphone-15-plus/article/Apple-iPhone-15-lineup-color-lineup-geo-230912_big.jpg.large.jpg",
   },
   {
-    title: "Aceternity UI",
-    link: "https://ui.aceternity.com",
+    title: "iPhone 15 Pro Max",
+    link: "https://iplanet.one/products/iphone-15-pro-mu793hn-a",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+      "https://www.apple.com/newsroom/videos/iphone-15-pro-action-button/posters/Apple-iPhone-15-Pro-lineup-Action-button-230912.jpg.large_2x.jpg",
   },
   {
-    title: "Tailwind Master Kit",
+    title: "iPhone 15 Pro",
     link: "https://tailwindmasterkit.com",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8opwV050zwLxgD-IrSL61hyxPCUioZ8u8Iw&s",
   },
   {
-    title: "SmartBridge",
-    link: "https://smartbridgetech.com",
+    title: "iPhone 13 mini",
+    link: "https://www.apple.com/newsroom/2021/09/apple-introduces-iphone-13-and-iphone-13-mini/",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+      "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_iphone13_hero_09142021_inline.jpg.large.jpg",
   },
   {
-    title: "Renderwork Studio",
-    link: "https://renderwork.studio",
+    title: "HomePod",
+    link: "https://www.apple.com/in/shop/buy-homepod/homepod",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/homepod-select-202210_FMT_WHH?wid=640&hei=600&fmt=jpeg&qlt=90&.v=1670557209437",
   },
 
   {
-    title: "Creme Digital",
-    link: "https://cremedigital.com",
+    title: "Studio display",
+    link: "https://www.apple.com/in/shop/buy-mac/apple-studio-display",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/studio-display-og-202203?wid=1200&hei=630&fmt=jpeg&qlt=95&.v=1645125507093",
   },
   {
-    title: "Golden Bells Academy",
-    link: "https://goldenbellsacademy.com",
+    title: "AirPods Max",
+    link: "https://www.apple.com/in/shop/buy-airpods/airpods-max/space-grey",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/airpods-max-select-spacegray-202011_FV1_FMT_WHH?wid=940&hei=800&fmt=jpeg&qlt=90&.v=1604776025000",
   },
   {
-    title: "Invoker Labs",
-    link: "https://invoker.lol",
+    title: "AirPods Pro",
+    link: "https://www.apple.com/in/shop/product/MTJV3HN/A/airpods-pro",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+      "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/MTJV3?wid=1144&hei=1144&fmt=jpeg&qlt=90&.v=1694014871985",
   },
   {
-    title: "E Free Invoice",
-    link: "https://efreeinvoice.com",
+    title: "Apple watch",
+    link: "https://www.apple.com/in/shop/buy-watch/apple-watch-se/44mm-cellular-midnight-aluminium-midnight-sport-band-sm-se",
     thumbnail:
-      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+      "https://store.storeimages.cdn-apple.com/1/as-images.apple.com/is/MT3D3ref_VW_34FR+watch-case-44-aluminum-midnight-cell-se_VW_34FR+watch-face-44-aluminum-midnight-se_VW_34FR?wid=752&hei=720&bgc=fafafa&trim=1&fmt=p-jpg&qlt=80&.v=ajJYOEQxYjNlejNwbWNnSU16d0NYaWhSbVIzRFJTYlp1MEk4OWNDaTcvNTlEbzMrd1Z5SUpEd0hiT0ZLRlZGNHVDTzRRaC84T1VMbXJRN05SRldIelBRWnNWZWtLcTZCYVRER3FsWWowaTk5RG8zK3dWeUlKRHdIYk9GS0ZWRjR4cVNUNDJadDNVSmRncE9SalAvZ24zZmdHMUt6VFlqa3BpV2VBOUNycGdrcDIxSk5peW5HTWQ0c004MmJwMkNtdGl6SHg4ZE5NYmlWSVQ5akRTdGpCZkNDUmN0SlpnYXpQNFNTbUVsbTlnST0=",
   },
 ];
